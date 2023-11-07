9485b2d1d249f302885340dbee7163d9e3857b60
LuisFelipe <felipe.hvesga@outlook.es>
2023-11-07 09:18:33 -0500

    AgendamientoApi

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\API\AgendamientoApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,7 @@ use App\Http\Controllers\AuthController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+ Route::apiResource('Agendamiento',AgendamientoApiController::class );
 
 Route::group([
     'prefix' => 'auth'
